43935fa9a90784f16066b3a290731da762e363d5
kisb01 <kisb01@gmail.com>
2021-04-29 11:19:49 +0200

    Complete file project

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,41 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        List<String> fullContacts = getListOfStringsToWrite(contacts);
+        StringBuilder sb = new StringBuilder();
+        concatFullContacts(fullContacts, sb);
+        writeToFile(output, sb);
+    }
+
+    private void writeToFile(String output, StringBuilder sb) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+            bw.write(sb.toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void concatFullContacts(List<String> fullContacts, StringBuilder sb) {
+        for (String contact : fullContacts) {
+            sb.append(contact).append(System.lineSeparator());
+        }
+    }
+
+    private List<String> getListOfStringsToWrite(Map<String, String> contacts) {
+        List<String> fullContacts = new ArrayList<>();
+        for (Map.Entry<String, String> contact : contacts.entrySet()) {
+            fullContacts.add(contact.getKey() + ": " + contact.getValue());
+        }
+        return fullContacts;
+    }
+}
